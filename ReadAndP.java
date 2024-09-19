1f7efb839ab39aa41d124db45f5f59530cea1c4e
LalitUp <147649609+LalitUp@users.noreply.github.com>
2024-09-19 07:45:07 +0000

    day1

---
+++ ReadAndP.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReadAndP {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("Enter the integer Value: ");
+        int num = sc.nextInt();
+
+        System.out.println("The Entered Value is: " + num);
+
+    }
+}
---
+++ Swaap_2_Numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Swaap_2_Numbers {
+
+    static void swapValue(int m, int n){
+        int temp = m;
+        m = n;
+        n = temp;
+        System.out.println("Value of m is" + m +" and Value of n is"+n);
+    }
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+
+    }
+    
+
+
+}
